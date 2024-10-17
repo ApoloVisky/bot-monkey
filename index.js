61950b81eb76b44aa5182f6b7bbb903ca32936bb
Adeilton da Silva Polovodoff <paraminhaalegria000@gmail.com>
2024-10-16 23:26:33 -0300

    erro

--- index.js
+++ index.js
@@ -103,7 +103,6 @@ distube.on("finishSong", (queue, song) => {
 
 distube.on("error", (channel, error) => {
   console.error("Erro no DisTube:", error);
-  if (channel) channel?.send("Ocorreu um erro ao tentar reproduzir a música.");
 });
 
 client.login(process.env.DISCORD_TOKEN);
