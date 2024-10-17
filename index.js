d3e00ba72ace88a1d0716215d2b1947f6bf524f5
Adeilton da Silva Polovodoff <paraminhaalegria000@gmail.com>
2024-10-16 23:02:47 -0300

    corrigindo erro da vm

--- index.js
+++ index.js
@@ -83,11 +83,12 @@ client.on("interactionCreate", async (interaction) => {
 distube.on("playSong", (queue, song) => {
   if (!queue || !song) {
     console.log("Erro: Não foi possível obter a fila ou a música.");
+    console.log("Erro: Não foi possível obter a fila ou a música.");
     return;
   }
 
   console.log(`Tocando agora: ${song.name}`);
-  queue.textChannel?.send(`🎶 Tocando agora: **${song.name}**`);
+ // queue.textChannel?.send(`🎶 Tocando agora: **${song.name}**`);
 });
 
 
